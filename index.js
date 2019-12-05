cb24413b9732d2e1e86fdc571be0a64240f04507
Oksana Kril <oksanakril@Oksanas-iMac.local>
2019-12-04 19:03:21 -0600

    3rd request by ID done

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.post("/api/users", (req, res) => {
           console.log(user)
         })
         .catch(() => {
-          res.status(500).json({errorMessage: "There was an error while saving the user to the database"})
+          res.status(500).json({ errorMessage: "There was an error while saving the user to the database" })
         });
     }
 })
@@ -42,20 +42,21 @@ app.get("/api/users", (req, res) => {
         })
 })
 
-// app.get("/api/users", (req, res) => {
-//    res.json(db) 
-// })
+app.get("/api/users/:id", (req, res) => {
+    if(req.params.id){
+        data
+            .findById(req.params.id)
+            .then(user =>{
+                res.status(201).json(user)
+            })
+            .catch(() => {
+                res.status(500).json({ errorMessage: "The user information could not be retrieved." })
+            })
+    }else{
+        res.status(404).json({ message: "The user with the specified ID does not exist." })
+    }
+})
 
-// app.get("/users/:id", (req, res) => {
-//     // req.params.id
-//     const user = db.find(row => row.id === req.params.id)
-    
-//     if (user){
-//         res.json(user)
-//     }else{
-//         res.status(404).json({ error: "User not found" })
-//     }
-// })
 
 // app.delete("/users/:id", (req, res) => {
 //     const user = db.find(row => row.id === req.params.id)
